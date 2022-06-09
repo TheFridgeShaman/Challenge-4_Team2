ea10570340677b26a3048a4e65bb277bcb7daefa
Andrea Spanu <Andrea.Spanu@arup.com>
2022-06-09 15:02:34 +0100

    added argparse

--- code/search.py
+++ code/search.py
@@ -1,22 +1,47 @@
+import argparse
 from importlib.resources import path
 from ifcopenshell import geom
 import pathlib
-from utils import Utils 
+from pathlib import Path
+from utils import Utils as ut
 
-ut = Utils()
-graph = ut.createGraph()
+def main(input_folder, output_path):
 
-path = pathlib.Path().parent.resolve() / "resources"
-files = pathlib.Path(path)
+	# Create empty list for the bounding boxes
+	boundingBoxes = []
+	# Create empty graph
+	graph = ut.createGraph()
+	
+	# Get path, get files from the directory
+	#folder = pathlib.Path().parent.resolve() / "resources"
+	path = pathlib.Path(input_path)
+	files = [f for f in path.iterdir() if ".ifc" in f.name.lower()]
+	# Open all files in the directory
+	# Generate bounding boxes for all ifc products and add them to the list
+	for f in files:
+		print("Parsing " + f.name + "...")
+		boundingBoxes.extend(ut.createBoundingBoxes(path,f))  
 
-boundingBoxes = []
+	# Collision check
+	ut.collisionCheck(boundingBoxes, graph)
+	
+	# Creation of the graph
+	graph.serialize(destination=output_path, format='turtle')
 
-for file in files.iterdir():
-  print(file.name)
-  
-  boundingBoxes.extend(ut.createBoundingBoxes(path, file))  
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i","--input_folder",
+						required = True,
+						help = "Path to the folder containing the ifc files",
+						type = str)
+	parser.add_argument("-o","--output_file",
+                        required = True,
+                        help = "Path to the output file",
+                        type = str)
 
-# Colision
-ut.collisionCheck(boundingBoxes, graph)
+	args = parser.parse_args()
+	
+	input_folder = args.input_folder
+	output_path = args.output_file
 
-graph.serialize(destination='C:/Users/atn/Desktop/output.ttl', format='turtle')
+	main(input_path, output_path)
